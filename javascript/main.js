821ee8cf052f932536f72371fc4b80426033a791
hzj04473 <hzj04473@naver.com>
2024-11-23 17:35:05 +0900

    231123. 변수

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+// console.log('hello javascript');
+// console.log(console);
+
+// var, let , const
+
+// var
+// for (var i = 0; i < 10; i++) {
+//   var name = 'scalper';
+//   var engName = 'codeScalper';
+// }
+
+// console.log(name, engName);
+
+// console.log(window);
+
+// let
+// let score = 0;
+// score = score + 1;
+// console.log(score);
+
+// const
+// const name = 'scalper';
+// name = 'codeScalper';
+// console.log(name);
+
+// 블럭 내에서 선언된 변수는 블랙 밖에서 사용이 불가능
+
+// {
+//   let eng = 'scalper';
+// }
+
+// console.log(eng); // error Uncaught ReferenceError: eng is not defined
+
+// 블럭 빡에서 선언된 변수는 블랙 내에서 사용이 가능
+// let eng = 'scalper';
+// {
+//   console.log(eng);
+// }
