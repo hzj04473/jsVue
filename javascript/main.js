169184276c9fb1103ecf51deb922f9f7aed6c7d1
hzj04473 <hzj04473@naver.com>
2024-11-24 22:53:53 +0900

    241124 함수

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,74 @@
-// array
-const animal = 'lion';
-const animal2 = 'rabbit';
+const item1 = 3000;
+const item2 = 2000;
+const item3 = 5000;
 
-const animals = ['lion', 'rabbit', 'monkey', 'dog', 'cat'];
+const royalty = 1000;
 
-// animals[3] = 'cheeta';
-// animals.push('tiger'); // 맨 뒤에
-// animals.unshift('dog'); // 맨 앞에
-// animals.pop(); // 맨 뒤에 뺴기
+// console.log(item1 + loyalty);
+// console.log(item2 + loyalty);
+// console.log(item3 + loyalty);
 
-const fruits = 'apple, banana, strawberry';
+// Functions
+// function showPrice(price) {
+//   // console.log(price + royalty);
+//   return price + royalty;
+// }
 
-// console.log(Array.isArray(animals));
-// console.log(Array.isArray(fruits));
+// const price1 = showPrice(item1);
+// const price2 = showPrice(item2);
+// const price3 = showPrice(item3);
 
-console.log(animals.indexOf('monkey'));
-console.log(animals.indexOf('rabbit'));
+// console.log(price1, price2, price3);
 
-// splice, slice
-// splice
-// animals.splice(animals.indexOf('monkey'), 3);
-console.log(animals);
-// slice
-const slicedAnimal = animals.slice(0, 2);
-console.log(slicedAnimal, animals);
+// 함수는 변수에 넣어서...
+// const showPrice = function (price) {
+//   return price + royalty;
+// };
 
-// object
-const monkey = {
-  name: 'kiki',
-  age: 5,
-  weight: 40,
-  gender: 'male',
-  iscute: true,
-  food: ['banana', 'carrot', 'potato'],
-  home: {
-    location: 'zoo',
-    size: 'big',
-    price: 3000,
+// console.log(showPrice(item1));
+// console.log(showPrice(item2));
+// console.log(showPrice(item3));
+
+// 화살표 함수
+// const showPrice = (price) => {
+//   console.log(price + royalty);
+// };
+
+// showPrice(item1);
+// showPrice(item2);
+// showPrice(item3);
+
+// 일반 함수와 화살표 함수의 차이점
+// 일반 함수는 함수 실행의 위치가 아무것에서 실행 가능하나
+// 화살표 함수는 함수 선언 뒤에서 실핼 되게 해야 함.
+// showPrice1(item1);
+// function showPrice1(price) {
+//   console.log(price + royalty);
+// }
+
+// showPrice2(item1);
+// Uncaught ReferenceError: Cannot access 'showPrice2' before initialization
+// const showPrice2 = (price) => {
+//   console.log(price + royalty);
+// };
+// showPrice2(item1);
+
+// console.log(this);
+const obj = {
+  name: 'test',
+  test1: function () {
+    console.log(this);
+  },
+  test2: () => {
+    console.log(this);
   },
 };
 
-console.log(monkey);
+// obj.test1();
+// obj.test2();
 
-monkey.gender = 'female';
+const a = obj.test1;
+const b = obj.test2;
 
-console.log(monkey['name']);
-console.log(monkey.weight);
-
-// json
-// console.log(monkey);
-const monkeyJSON = JSON.stringify(monkey); // JSON 화
-const parsedJSON = JSON.parse(monkeyJSON); // JSON 데이터를 사용해서 쓸 수 있도록 변환
-console.log(parsedJSON);
+a();
+b();
